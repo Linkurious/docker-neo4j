abe3934bb9b635e9e9f662d9022fb146c851c2aa
Thibaut Vanderhaegen <thibaut.vanderhaegen@linkurio.us>
2023-06-29 11:16:38 +0200

    comment

--- scripts/dataset-download.sh
+++ scripts/dataset-download.sh
@@ -156,6 +156,7 @@ function restore_database {
 
     neo4j_restore_params=(restore \
          --from="$RESTORE_FROM" \
+         # do not fix lint quote on force_flag otherwise we have errors on script
          --database="$db" $FORCE_FLAG \
          --to-data-directory "${data_folder_prefix}/data/databases/" \
          --to-data-tx-directory "${data_folder_prefix}/data/transactions/" \
